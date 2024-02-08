fb9ea96c77b9977398b6630bb2954abdb72a2b8e
abdulwahid5605 <abdulwahid4888888@gmail.com>
2024-02-08 14:20:04 -0800

    Files Integrated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,11 +3,19 @@ import Hero from "../Components/Home/Hero";
 import Card from "../Components/Home/Card";
 import Location from "../Components/Home/Location";
 import SecurityServices from "../Components/Home/SecurityServices";
+import WhyChoose from "../Components/WhyChoose";
+import EmailComponent from "../Components/EmailComponent";
+import OfferComponent from "../Components/OfferComponenet";
+import Testimonials from "../Components/Testimonials";
 
 const Home = () => {
   return (
     <Fragment>
       <Hero />
+      <WhyChoose />
+      <EmailComponent />
+      <OfferComponent />
+      <Testimonials />
       <Card />
       <Location />
       <SecurityServices />
